0d8f5c64b0a994d75814f73b11649eff4f7260e2
G3N3T1C <45001982+FerrarioChristian@users.noreply.github.com>
2021-05-25 21:57:52 +0200

    Event Handler Major Bug Fix

--- EventHandler.py
+++ EventHandler.py
@@ -5,24 +5,26 @@ from watchdog.events import FileSystemEventHandler
 from Logger import Logger
 
 class EventHandler(FileSystemEventHandler):
-    def __init__(self, observed_folder, destination_folder):
+    def __init__(self, observed_folder):
         self.observed_folder = observed_folder
-        self.destination_folder = destination_folder
         self.json_dict = load(open(r'C:\Users\chrif\Documents\Developing\PythonFileSorter\destinations.json'))
         self.logger = Logger()
+        self.found = False
 
     def on_modified(self, event):
         for file_name in listdir(self.observed_folder):
+            self.found = False
             src = self.observed_folder + '/' + file_name
             if  file_name.split('.')[1] not in self.json_dict['Ignore']:
                 for destination, path in self.json_dict['destinations'].items():
                     if file_name.split('.')[1] in self.json_dict[destination]:
                         dest = path + file_name
                         move(src, dest)
-                        self.logger.log("File " + file_name + " moved in " + destination +" (?)")
+                        self.found = True
+                        self.logger.log("File " + file_name + " moved in " + dest +" (?)")
                         break
-                    else: 
-                        dest = self.json_dict['destinations']['Downloads'] + file_name
-                        move(src, dest)
-                        self.logger.log("File " + file_name + " moved in " + destination +" (?)")
-                        break
+                if(self.found == False):
+                    dest = self.json_dict['destinations']['Default'] + file_name
+                    move(src, dest)
+                    self.logger.log("File " + file_name + " moved in " + dest +" (?)")
+                    break
